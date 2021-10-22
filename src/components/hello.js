e6bb7e5e72d3d9350d6d61fd5bcb74f6f34ab121
Tanya <Leenders>
2021-10-22 12:17:45 +0200

    use of proptypes

--- src/components/hello.js
+++ src/components/hello.js
@@ -1,4 +1,12 @@
 import React from "react";
+import PropTypes from "prop-types";
+
+Hello.PropTypes = {
+    name: PropTypes.string.isRequired,
+};
+// Useful when working in team: Defines which props are used and from wich type + required or not
+// This will give an error in the console if not respected
+
 const Hello = ({name}) => {
     return (
         <>
