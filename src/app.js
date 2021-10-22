e0fa9fe95046c9bdbae64b3eef57765683f2d173
Tanya <Leenders>
2021-10-22 12:24:47 +0200

    adding bulma

--- src/app.js
+++ src/app.js
@@ -1,5 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import "bulma";
 import Hello from "./components/hello";
 
 ReactDOM.render(<Hello name={"Name"} />, document.querySelector("#app"));
--- src/components/hello.js
+++ src/components/hello.js
@@ -1,12 +1,6 @@
 import React from "react";
 import PropTypes from "prop-types";
 
-Hello.PropTypes = {
-    name: PropTypes.string.isRequired,
-};
-// Useful when working in team: Defines which props are used and from wich type + required or not
-// This will give an error in the console if not respected
-
 const Hello = ({name}) => {
     return (
         <>
@@ -14,4 +8,10 @@ const Hello = ({name}) => {
         </>
     );
 };
+
+Hello.propTypes = {
+    name: PropTypes.string.isRequired,
+};
+// Useful when working in team: Defines which props are used and from wich type + required or not
+// This will give an error in the console if not respected
 export default Hello;
